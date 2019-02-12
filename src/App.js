6bce83dbaba2837538dae9391a3b5ff6ad8b3aee
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2019-02-12 16:58:18 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { PDFExport } from '@progress/kendo-react-pdf';
+import './App.css';
+
+class App extends Component {
+  resume;
+  
+//   state = {
+//     users: [];
+//   }
+//
+//   componentDidMount() {
+//     fetch("rails server")
+//         .then(res => res.json())
+//         .then(users => this.setState({users: users.map(user => ({...user, exists: false}))}
+// }
+  
+  exportPDF = () => {
+    this.resume.save();
+  }
+  
+  render() {
+    
+    return (
+      <div className="App">
+        <div>
+          <button onClick={this.exportPDF}>Download</button>
+        </div>
+        <PDFExport paperSize={'Letter'} fileName="{user.name}.pdf" title="" subject="" keywords="" ref={(r) => this.resume = r}>
+          
+          <div style={{height: 792, width: 612, padding: 'none', backgroundColor: 'white', boxShadow: '5px 5px 5px black', margin: 'auto',
+            overflowX: 'hidden', overflowY: 'hidden'}} >user.bio</div>
+          
+          
+        </PDFExport>
+      </div>
+    );
+  }
+}
+
+export default App;
